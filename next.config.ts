06cd0ad510e31be9aabb5759cf67e9580401418c
Jorge Pires <jorge.business@outlook.com.br>
2026-07-21 22:28:50 -0300

    fix: Configure the files to fix the onclick error on the npm run dev website (Network:       http://192.168.1.4:3000)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  allowedDevOrigins: [
+    "192.168.1.4",
+    "192.168.1.4:3000",
+  ],
+}
 
-export default nextConfig;
+export default nextConfig
