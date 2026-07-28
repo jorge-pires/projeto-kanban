0b5ea9883badc5bb1996e7406dfbc6b483ef13cc
Jorge Pires <jorge.business@outlook.com.br>
2026-07-28 16:29:28 -0300

    refactor: extract navigation link data

---
+++ data/navigation.ts
@@ -0,0 +1,19 @@
+export interface NavigationLink {
+  label: string
+  href: string
+}
+
+export const navigationLinks: NavigationLink[] = [
+  {
+    label: "Início",
+    href: "#home",
+  },
+  {
+    label: "Recursos",
+    href: "#features",
+  },
+  {
+    label: "Sobre",
+    href: "#about",
+  },
+]
