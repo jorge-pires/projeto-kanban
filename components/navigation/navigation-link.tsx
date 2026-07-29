84402e3bf30df3b5ed6d3f1ac7d9c55eec21ebc0
Jorge Pires <jorge.business@outlook.com.br>
2026-07-29 11:11:15 -0300

    feat: support external navigation links

--- components/navigation/navigation-link.tsx
+++ components/navigation/navigation-link.tsx
@@ -6,21 +6,25 @@ import type {
 interface NavigationLinkProps
   extends AnchorHTMLAttributes<HTMLAnchorElement> {
   children: ReactNode
+  external?: boolean
 }
 
 export function NavigationLink({
   children,
   className,
+  external = false,
   ...props
 }: NavigationLinkProps) {
   return (
     <a
+      {...props}
+      target={external ? "_blank" : props.target}
+      rel={external ? "noopener noreferrer" : props.rel}
       className={`
         transition
         hover:text-blue-600
         ${className ?? ""}
       `}
-      {...props}
     >
       {children}
     </a>
