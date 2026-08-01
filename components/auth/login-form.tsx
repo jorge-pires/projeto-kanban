c0b35f2f948ef9700bdd125e73233544fe29a0b6
Jorge Pires <jorge.business@outlook.com.br>
2026-07-31 21:02:01 -0300

    feat: add login form component

---
+++ components/auth/login-form.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import { useState } from "react"
+
+import { Button } from "@/components/ui/Button"
+
+export function LoginForm() {
+  return (
+    <form>
+      {/* Campos serão adicionados aqui */}
+    </form>
+  )
+}
